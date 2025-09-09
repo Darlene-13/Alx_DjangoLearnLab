0597eb1d99228a3e07912beeeb2c7e4d3ca662a3
Darlene-13 <darlenenasimiyu@gmail.com>
2025-09-09 19:27:50 +0300

    Fixed import

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,8 +4,12 @@ from relationship_app import views
 from relationship_app.views import LibraryDetailView
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import LibraryDetailView, list_books
+
+from .views import list_books
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
