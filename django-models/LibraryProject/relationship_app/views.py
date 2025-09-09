87c310061e98d60814a33bf5587727358a62350e
Darlene-13 <darlenenasimiyu@gmail.com>
2025-09-09 18:44:07 +0300

    Adusted the template location

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from relationship_app.models import Author, Book, Library, Librarian
 
-# Create your views here.
+
+# Function based views to list all books stored in the databas
+"""View to list all books in the database.
+URL: /books/
+"""
+@staticmethod
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'title': 'List of Books', 'books': books, 'author': Author, 'library': Library, 'librarian': Librarian})
+
+# class based views to display details for a specific library
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    template_path = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.books.all()
+        context['librarian'] = self.object.librarian
+        return context
+    
