eb23483f6870a8db9a3a9871b393e0bd5bfb921c
Darlene-13 <darlenenasimiyu@gmail.com>
2025-09-09 23:46:32 +0300

    Configured links to urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,15 +4,15 @@ from relationship_app import views
 from relationship_app.views import LibraryDetailView
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import LibraryDetailView, list_books
+from .views import LibraryDetailView, list_books, LoginView, LogoutView, RegisterView
 
 from .views import list_books
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('register/', views.register_view, name='register'),
+    path('login/',  LoginView.as_view(template_name='relationship_app/templates/relationship_app/login.html') , name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/templates/relationship_app/logout.html'), name='logout'),
+    path('register/',RegisterView.as_view(template_name='register'),)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
